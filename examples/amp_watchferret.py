586af5708343ba4d7261d9677595fb140fb7a27e
Dylan Sperrer <82551072+p0t4t0sandwich@users.noreply.github.com>
2022-12-13 20:30:37 -0600

    Update amp_watchferret.py

--- examples/amp_watchferret.py
+++ examples/amp_watchferret.py
@@ -19,39 +19,38 @@ def watchdog(instance_name):
     )
     ADS.initHandler()
 
-    target = (ADS.ADSModule.GetInstances())["result"][0]
+    for target in (ADS.ADSModule.GetInstances())["result"]:
+        for instance in target["AvailableInstances"]:
+            if instance["InstanceName"] == instance_name:
+                instance_id = instance["InstanceID"]
 
-    for instance in target["AvailableInstances"]:
-        if instance["InstanceName"] == instance_name:
-            instance_id = instance["InstanceID"]
+                InstanceAPI = ADS.InstanceLogin(instance_id=instance_id)
 
-            InstanceAPI = ADS.InstanceLogin(instance_id=instance_id)
+                if InstanceAPI != None:
+                    InstanceAPI.init()
 
-            if InstanceAPI != None:
-                InstanceAPI.init()
+                    state_tracker = 0
+                    while True:
+                        try:
+                            status = InstanceAPI.Core.GetStatus()["State"]
 
-                state_tracker = 0
-                while True:
-                    try:
-                        status = InstanceAPI.Core.GetStatus()["State"]
+                            if status == 30: state_tracker += 1
+                            else: state_tracker = 0
 
-                        if status == 30: state_tracker += 1
-                        else: state_tracker = 0
+                            if state_tracker >= 2:
+                                now = str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+                                print(f"Watchdog Event Detected: {now}")
+                                InstanceAPI.Core.Kill()
+                                sleep(10)
+                                InstanceAPI.Core.Start()
+                                print(f"Instance {instance_name} has been rescued")
 
-                        if state_tracker >= 2:
-                            now = str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-                            print(f"Watchdog Event Detected: {now}")
-                            InstanceAPI.Core.Kill()
-                            sleep(10)
-                            InstanceAPI.Core.Start()
-                            print(f"Instance {instance_name} has been rescued")
+                            sleep(300)
+                        except:
+                            InstanceAPI.Login()
 
-                        sleep(300)
-                    except:
-                        InstanceAPI.Login()
-
-            else:
-                print(f"Instance Offline: {instance_name}")
+                else:
+                    print(f"Instance Offline: {instance_name}")
 
 if __name__ == "__main__":
     watchdog("the non-friendly instance name1")

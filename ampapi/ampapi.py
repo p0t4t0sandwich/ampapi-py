59e0b06ba84cbf1b3a631042f88ceb2a62fcb799
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-05-03 21:28:39 -0600

    Quick fix for APICallAsync

--- ampapi/ampapi.py
+++ ampapi/ampapi.py
@@ -66,7 +66,7 @@ class AMPAPI():
         data_json = json.dumps(data_added)
 
         async with ClientSession() as session:
-            async with session.post(url=f'{self.dataSource}{endpoint}', headers=headers, data=data_json) as post:
+            async with session.post(url=f'{self.dataSource}/{endpoint}', headers=headers, data=data_json) as post:
                 response = await post.json()
                 post.close()
 
@@ -4612,7 +4612,7 @@ class AMPAPIHandler(AMPAPI):
             data_json = json.dumps(data_added)
 
             async with ClientSession() as session:
-                async with session.post(url=f'{self.dataSource}{endpoint}', headers=headers, data=data_json) as post:
+                async with session.post(url=f'{self.dataSource}/{endpoint}', headers=headers, data=data_json) as post:
                     response = await post.json()
                     post.close()
 
